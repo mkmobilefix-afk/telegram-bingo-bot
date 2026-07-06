2b76d93ab37c5bf84f40b6de13eda8a363aa88f0
mkmobilefix-afk <mkmobilefix@gmail.com>
2026-07-06 15:39:05 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI(title="Ethio Bingo")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "title": "Ethio Bingo",
+            "entry_fee": "20 Birr"
+        },
+    )
