00b398ac39a80a7f6f6fdcc36d59be813d157cf9
mkmobilefix-afk <mkmobilefix@gmail.com>
2026-07-06 16:50:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from telegram import Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+)
+from contextlib import asynccontextmanager
+from config import BOT_TOKEN
 
-app = FastAPI()
+bot = Application.builder().token(BOT_TOKEN).build()
 
-templates = Jinja2Templates(directory="templates")
-
-@app.get("/", response_class=HTMLResponse)
-async def home(request: Request):
-    return templates.TemplateResponse(
-        "index.html",
-        {"request": request}
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "🇪🇹 Welcome to Ethio Bingo!\n\n"
+        "🎟 Entry Fee: 20 Birr"
     )
+
+bot.add_handler(CommandHandler("start", start))
