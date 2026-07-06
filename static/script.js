d0882959a854edf11d00a316058950010db2693a
mkmobilefix-afk <mkmobilefix@gmail.com>
2026-07-06 23:11:33 +0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,13 @@
+Telegram.WebApp.ready();
 
+function deposit(){
+    Telegram.WebApp.showAlert(
+        "Send your Telebirr payment screenshot."
+    );
+}
+
+function play(){
+    Telegram.WebApp.showAlert(
+        "Game will start soon."
+    );
+}
