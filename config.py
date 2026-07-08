4f7962afc2895725c5b874659946bfbf4ce7cdf3
mkmobilefix-afk <mkmobilefix@gmail.com>
2026-07-08 07:29:03 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,20 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
 ADMIN_ID = 474362089
 
 MIN_DEPOSIT = 100
 MAX_DEPOSIT = 2000
 
 CARD_PRICE = 20
-
 MAX_CARDS = 5
 
 TELEBIRR_NUMBER = "0905047464"
+
+DB_NAME = "bingo.db"
+
+WEBAPP_URL = os.getenv(
+    "WEBAPP_URL",
+    "https://your-app.onrender.com"
+)
